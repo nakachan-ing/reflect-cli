a07e4a681fd32b07be5a76d5d48989feaa7bd02a
nakachan-ing <79064717+nakachan-ing@users.noreply.github.com>
2025-04-08 16:27:41 +0900

    ♻️ Update success comment

--- internal/store/jsonstore/writer.go
+++ internal/store/jsonstore/writer.go
@@ -58,14 +58,14 @@ func InsertTagToJson(tags []model.Tag, tag model.Tag, config *model.Config) erro
 func SaveUpdatedJson[T any](v []T, jsonPath string) error {
 	updatedJson, err := json.MarshalIndent(v, "", "  ")
 	if err != nil {
-		return fmt.Errorf("❌ Failed to convert to JSON: %w", err)
+		return fmt.Errorf("failed to convert to JSON: %w", err)
 	}
 
 	err = os.WriteFile(jsonPath, updatedJson, 0644)
 	if err != nil {
-		return fmt.Errorf("❌ Failed to write JSON file: %w", err)
+		return fmt.Errorf("failed to write JSON file: %w", err)
 	}
 
-	log.Printf("✅ Successfully updated JSON file: %s", jsonPath)
+	fmt.Printf("✅ Successfully updated JSON file: %s", jsonPath)
 	return nil
 }
