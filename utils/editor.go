aefcd4465f86b85acc5e36aabdd860530dbf325e
nakachan-ing <79064717+nakachan-ing@users.noreply.github.com>
2025-04-10 02:24:38 +0900

    ⚡️ Add sleep time before opening editor

--- utils/editor.go
+++ utils/editor.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"time"
 
 	"github.com/nakachan-ing/reflect-cli/model"
 )
 
 func OpenEditor(filePath string, config model.Config) error {
+	time.Sleep(2 * time.Second)
 	c := exec.Command(config.Editor, filePath)
 	c.Stdin = os.Stdin
 	c.Stdout = os.Stdout
